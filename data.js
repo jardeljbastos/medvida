8adb04a76ca7b45fb07c3465bfb7523ba898c51a
realidademax <84300151+realidademax@users.noreply.github.com>
2021-10-07 08:26:42 -0300

    Initial commit

---
+++ data.js
@@ -0,0 +1,448 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-analysisa",
+      "name": "AnalysisA",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.0211436995579497,
+        "pitch": -0.2954663630398322,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.365278702470537,
+          "pitch": 0.20117920356501173,
+          "rotation": 5.497787143782138,
+          "target": "4-analysise"
+        },
+        {
+          "yaw": -0.8678578221489364,
+          "pitch": 0.19940231915039774,
+          "rotation": 0.7853981633974483,
+          "target": "3-analysisd"
+        },
+        {
+          "yaw": 1.3296067974617962,
+          "pitch": 0.19088191993886028,
+          "rotation": 5.497787143782138,
+          "target": "1-analysisb"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-analysisb",
+      "name": "AnalysisB",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.1588025527306325,
+        "pitch": -0.010207460419536574,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5100811934086398,
+          "pitch": 0.2770808387323722,
+          "rotation": 0,
+          "target": "3-analysisd"
+        },
+        {
+          "yaw": -1.4914342711056996,
+          "pitch": 0.051399628021957966,
+          "rotation": 8.63937979737193,
+          "target": "2-analysisc"
+        },
+        {
+          "yaw": -2.4174307360761293,
+          "pitch": 0.20319700282340847,
+          "rotation": 5.497787143782138,
+          "target": "4-analysise"
+        },
+        {
+          "yaw": 2.501174302018585,
+          "pitch": 0.2599215684118299,
+          "rotation": 0.7853981633974483,
+          "target": "0-analysisa"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-analysisc",
+      "name": "AnalysisC",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.3836004551810017,
+        "pitch": 0.021044049904242357,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8243666003230903,
+          "pitch": 0.3958237568988281,
+          "rotation": 3.141592653589793,
+          "target": "3-analysisd"
+        },
+        {
+          "yaw": 2.89780993490293,
+          "pitch": 0.10124605579870583,
+          "rotation": 3.9269908169872414,
+          "target": "1-analysisb"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-analysisd",
+      "name": "AnalysisD",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.1216343496678043,
+        "pitch": 0.19094897608861672,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.167900378374112,
+          "pitch": 0.12378879719015501,
+          "rotation": 9.42477796076938,
+          "target": "2-analysisc"
+        },
+        {
+          "yaw": 1.8415133019828636,
+          "pitch": 0.18235321362968193,
+          "rotation": 3.9269908169872414,
+          "target": "1-analysisb"
+        },
+        {
+          "yaw": 2.8976794123653944,
+          "pitch": 0.31983980969098624,
+          "rotation": 5.497787143782138,
+          "target": "0-analysisa"
+        },
+        {
+          "yaw": -2.8895519069429962,
+          "pitch": 0.23993884654391096,
+          "rotation": 3.141592653589793,
+          "target": "4-analysise"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-analysise",
+      "name": "AnalysisE",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.8879356801408242,
+        "pitch": 0.13929844772758315,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.485187206238198,
+          "pitch": 0.34026897544596224,
+          "rotation": 0.7853981633974483,
+          "target": "0-analysisa"
+        },
+        {
+          "yaw": 3.0949829238104547,
+          "pitch": 0.1377676389659186,
+          "rotation": 3.141592653589793,
+          "target": "3-analysisd"
+        },
+        {
+          "yaw": -2.749184055911531,
+          "pitch": 0.04682464589852131,
+          "rotation": 1.5707963267948966,
+          "target": "1-analysisb"
+        },
+        {
+          "yaw": -1.0425543519941574,
+          "pitch": 0.10599057601246997,
+          "rotation": 5.497787143782138,
+          "target": "5-analysisf"
+        },
+        {
+          "yaw": 1.9181074097275772,
+          "pitch": 0.3818091994110091,
+          "rotation": 0.7853981633974483,
+          "target": "8-analysisi"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-analysisf",
+      "name": "AnalysisF",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.8280697942426336,
+        "pitch": 0.3151099127227859,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9363713811726093,
+          "pitch": 0.37909191104648166,
+          "rotation": 3.9269908169872414,
+          "target": "4-analysise"
+        },
+        {
+          "yaw": 1.027141001933881,
+          "pitch": 0.06802360540486418,
+          "rotation": 1.5707963267948966,
+          "target": "0-analysisa"
+        },
+        {
+          "yaw": -1.7101524047662302,
+          "pitch": 0.06985887840509264,
+          "rotation": 0.7853981633974483,
+          "target": "6-analysisg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-analysisg",
+      "name": "AnalysisG",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 1.43529328856037,
+        "pitch": 0.03586698678712352,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.586485571477115,
+          "pitch": -0.044500881348895405,
+          "rotation": 3.141592653589793,
+          "target": "5-analysisf"
+        },
+        {
+          "yaw": 0.7878319363822506,
+          "pitch": -0.16554307294298098,
+          "rotation": 0,
+          "target": "7-analysish"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-analysish",
+      "name": "AnalysisH",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.6197221354330527,
+        "pitch": 0.032166518953324896,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7737375295222293,
+          "pitch": 0.20789999150934335,
+          "rotation": 5.497787143782138,
+          "target": "5-analysisf"
+        },
+        {
+          "yaw": -2.540511231190397,
+          "pitch": 0.06265815408715802,
+          "rotation": 9.42477796076938,
+          "target": "6-analysisg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-analysisi",
+      "name": "AnalysisI",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.6159278571145563,
+        "pitch": 0.017749933459562328,
+        "fov": 1.2634123859167636
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "HPM GE | Healthcare Project Manager",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
